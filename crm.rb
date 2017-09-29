df826e9136f305e08865a4a04e935ef4338846d0
Michelle Cinello <m.cinello@gmail.com>
2017-09-29 16:15:10 -0400

    added the rest of the call options

--- crm.rb
+++ crm.rb
@@ -1,3 +1,5 @@
+require_relative 'contact'
+
 class CRM
 
   def initialize
@@ -5,20 +7,52 @@ class CRM
   end
 
   def main_menu
-
+    while true # repeat indefinitely
+      print_main_menu
+      user_selected = gets.to_i
+      call_option(user_selected)
+    end
   end
 
   def print_main_menu
-
+    puts '[1] Add a new contact'
+    puts '[2] Modify an existing contact'
+    puts '[3] Delete a contact'
+    puts '[4] Display all the contacts'
+    puts '[5] Search by attribute'
+    puts '[6] Exit'
+    puts 'Enter a number: '
   end
 
-  def call_option
-
+  def call_option(user_selected)
+    case user_selected
+  when 1 then add_new_contact
+  when 2 then modify_existing_contact
+  when 3 then delete_contact
+  when 4 then display_all_contacts
+  when 5 then search_by_attribute
+  when 6 then abort("Goodbye!")
+  # Finish off the rest for 3 through 6
+  # To be clear, the methods add_new_contact and modify_existing_contact
+  # haven't been implemented yet
+  end
   end
 
   def add_new_contact
+    print 'Enter First Name: '
+  first_name = gets.chomp
 
-  end
+  print 'Enter Last Name: '
+  last_name = gets.chomp
+
+  print 'Enter Email Address: '
+  email = gets.chomp
+
+  print 'Enter a Note: '
+  note = gets.chomp
+
+  Contact.create(first_name, last_name, email, note)
+    end
 
   def modify_existing_contact
 
@@ -29,12 +63,20 @@ class CRM
   end
 
   def display_all_contacts
-    
+
   end
 
   def search_by_attribute
-    
+
   end
 
 
 end
+
+
+
+
+a_crm_app = CRM.new
+a_crm_app.main_menu
+
+a_crm_app.print_main_menu
